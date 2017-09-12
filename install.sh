3336066a353a794419556f957dc28b2265907eae
Armond Ave <armond.ave@papercut.com>
2017-09-12 10:47:48 +1000

    Added reset command and improved display of messages with the use of unicode characters

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@
 
 echo "Installing..."
 
+alias repox=./repox.sh
+
+echo "Successfully installed. Start using repox now"
+
 # TODO: Install repox.sh
--- repox.sh
+++ repox.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 #==================================================
 # CONSTANTS
@@ -12,26 +12,33 @@ PROFILE_FILE="$REPOX_DIR/profile.conf"
 COMMAND_ADD="add"
 COMMAND_MAIN="repox"
 COMMAND_LIST="list"
+COMMAND_RESET="reset"
 COMMAND_VIEW_STATUS="view"
+
+# others
+SEPARATOR="\n"
+ICON_ERROR="✗"
+ICON_SUCCESS="✓"
+ICON_WARN="⚠"
 
 #==================================================
 # FUNCTIONS
 #==================================================
 function init {
 	if [ ! -d "$REPOX_DIR" ]; then
-		echo "Creating repox directory"
 		mkdir $REPOX_DIR
+		echo "$ICON_SUCCESS Repox directory created."
 	fi
 
 	if [ ! -f "$PROFILE_FILE" ]; then
-		echo "Creating profile.conf"
 		touch $PROFILE_FILE
+		echo "$ICON_SUCCESS $(basename $PROFILE_FILE) file created."
 	fi
 }
 
 function register {
 	if ! git status >& /dev/null; then
-  		echo "error: This folder is not a repo."
+  		echo "$ICON_ERROR This folder is not part of a repo."
   		exitValue=1
   		return 1
 	fi
@@ -39,11 +46,11 @@ function register {
 	rootDir="$(git rev-parse --show-toplevel)"
 
 	if grep -q $rootDir $PROFILE_FILE; then
-        echo "This repo directory has already been added."
+        echo "$ICON_WARN This repo directory has already been added."
     else
-        echo "Successfully added repo directory:"
+        echo "$ICON_SUCCESS Successfully added repo directory:"
         printf "(+) "
-        (printf "$rootDir" | tee -a $PROFILE_FILE)
+        (printf "$rootDir$SEPARATOR" | tee -a $PROFILE_FILE)
 	fi
 }
 
@@ -52,9 +59,11 @@ function viewStatus {
 }
 
 function showUsage {
-	printf "\nUsage: repos {add|view}\n"
+	printf "\nUsage: $COMMAND_MAIN {$COMMAND_ADD|$COMMAND_LIST|$COMMAND_RESET|$COMMAND_VIEW_STATUS}\n"
 	echo "Commands:"
 	printf "\t$COMMAND_ADD\tMonitor current folder's parent repository for status and updates\n"
+	printf "\t$COMMAND_LIST\tList all repo directories being monitored\n"
+	printf "\t$COMMAND_RESET\tRemove all repo directories from monitoring\n"
 	printf "\t$COMMAND_VIEW_STATUS\tView status of monitored repositories\n"
 }
 
@@ -64,8 +73,10 @@ function fetchRepos {
 	for repo in "${repos[@]}"; do
         (
             cd $repo;
+            currentBranch=$(git rev-parse --abbrev-ref HEAD)
+            repoName=$(basename `git rev-parse --show-toplevel`)
             result=$(git fetch);
-            echo "Fetched $repo: $result"
+            echo "$repoName [$currentBranch]"
         ) &
 	done
 	wait
@@ -94,13 +105,20 @@ function loadRepoList {
     done
 }
 
+function reset {
+    rm -rf $REPOX_DIR
+    echo "$ICON_SUCCESS Removed all repo dirs from monitoring."
+}
+
 #==================================================
 # MAIN ENTRY
 #==================================================
 
-exitValue=0
 
-init
+if [ "$1" != "$COMMAND_RESET" ]; then
+    init
+fi
+
 case $1 in
 	$COMMAND_ADD )
 		register ;;
@@ -108,8 +126,10 @@ case $1 in
 		viewStatus ;;
     $COMMAND_LIST )
         listRepos ;;
+    $COMMAND_RESET )
+        reset ;;
 	* )
-		echo "error: Unknown command or no command supplied."
+		echo "$ICON_ERROR: Unknown command or no command supplied."
 		showUsage
 		exitValue=1 ;;
 esac
